3d0fcc0d2c3cb9d085ab5b6b9dcb134e063f1419
Aaraanit <arianittershnjaku@gmail.com>
2024-05-30 18:46:10 +0200

    30.05.2024

--- src/screens/AboutScreen.js
+++ src/screens/AboutScreen.js
@@ -1,12 +1,11 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text } from "react-native";
+import React from "react";
 
 function AboutScreen() {
   return (
     <View>
       <Text>AboutScreen</Text>
     </View>
-  )
+  );
 }
-
 export default AboutScreen;
